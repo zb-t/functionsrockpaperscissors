c9fdaea6467dbbffde8e79e0c2c498c682748a37
zb-t <64562253+zb-t@users.noreply.github.com>
2022-02-23 00:18:56 +0000

    game update

---
+++ sky-functions/exercise14/venv/include/isolatedattempt.py
@@ -0,0 +1,20 @@
+import random
+
+
+def computer_move():
+    computer_choice = random.randint(0,3)
+    if computer_move == 0:
+        print("The computer's weapon is ROCK!")
+    elif computer_move == 1:
+        print("The computer's weapon is PAPER!")
+    elif computer_move == 2:
+        print("The computer's weapon is SCISSORS!")
+    return
+
+
+
+for computer_move() in range(0, 3):
+    computer_move = random.randint(0, 3)
+    while number in lotterynumber:
+        number = random.randint(0, 50)
+    lotterynumber.append(number)
